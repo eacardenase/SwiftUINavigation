5747687c946c06f908167b777b2f7a13ea0f6dcb
Edwin Cardenas <eacardenase@gmail.com>
2026-04-25 12:56:59 -0500

    feat: returning back to root in deeply nested navigation stack

--- SwiftUINavigation/ContentView.swift
+++ SwiftUINavigation/ContentView.swift
@@ -7,23 +7,38 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    let number: Int
+    // @Binding var path: [Int]
+    @Binding var path: NavigationPath
+
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        // path.removeAll()
+                        // path = []
+                        path = NavigationPath()
+                    } label: {
+                        Label("Home", systemImage: "house")
+                    }
+                }
+            }
+    }
+}
+
 struct ContentView: View {
+    // @State private var path = [Int]()
     @State private var path = NavigationPath()
 
     var body: some View {
         NavigationStack(path: $path) {
-            VStack {
-
-            }
-            .toolbar {
-                Button("Push Number") {
-                    path.append(0320)
+            DetailView(number: 0, path: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path)
                 }
-
-                Button("Push String") {
-                    path.append("Hello, World!")
-                }
-            }
         }
     }
 }
