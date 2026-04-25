0cd6dfd3b8c7d74a1954dc687aaf5280897db0ef
Edwin Cardenas <eacardenase@gmail.com>
2026-04-25 13:00:12 -0500

    refactor: better representate deeply nested navigation stack

--- SwiftUINavigation/ContentView.swift
+++ SwiftUINavigation/ContentView.swift
@@ -9,17 +9,14 @@ import SwiftUI
 
 struct DetailView: View {
     let number: Int
-    // @Binding var path: [Int]
     @Binding var path: NavigationPath
 
     var body: some View {
-        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
+        NavigationLink("Go to next page", value: number + 1)
+            .navigationTitle("Page: \(number)")
             .toolbar {
                 ToolbarItem {
                     Button {
-                        // path.removeAll()
-                        // path = []
                         path = NavigationPath()
                     } label: {
                         Label("Home", systemImage: "house")
@@ -30,12 +27,12 @@ struct DetailView: View {
 }
 
 struct ContentView: View {
-    // @State private var path = [Int]()
     @State private var path = NavigationPath()
+    @State private var counter = 0
 
     var body: some View {
         NavigationStack(path: $path) {
-            DetailView(number: 0, path: $path)
+            DetailView(number: counter, path: $path)
                 .navigationDestination(for: Int.self) { i in
                     DetailView(number: i, path: $path)
                 }
